8b4bbedc2c8960edf4432fba25641d3f8e4665b2
Kai <b10223052@yuntech.org.tw>
2018-10-24 14:28:34 +0800

    create phaser file by webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import phaser from 'phaser'
+const config = {
+  width: 800,
+  height: 600,
+  parent: 'content',
+  // scene: [
+  //     Preloader
+  // ]
+}
+const game = new phaser.Game(config)
